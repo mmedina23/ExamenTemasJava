6da81fff94e0de5995d4ede86c1a516899b95d3e
mmedina23 <mariana.med23@gmail.com>
2024-10-08 00:36:43 +0200

    examen java

---
+++ src/main/java/com/examen/Examen.java
@@ -0,0 +1,42 @@
+package com.examen;
+
+public class Examen {
+    /**
+     * 
+     *  Llamar los metodos ejercicio1 y ejercicio2 desde el main
+     */
+    public static void main(String[] args) {
+       
+        
+
+    }
+    
+   /**
+    *
+    * Escribe un programa que muestre por consola (con un print) los
+    * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+    * cada impresión), sustituyendo los siguientes:
+    * - Múltiplos de 3 por la palabra "fizz".
+    * - Múltiplos de 5 por la palabra "buzz".
+    * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+    */
+    public void ejercicio1(){
+        
+    }
+    
+    /**
+    * Crea un programa que calcule quien gana más partidas al piedra,
+    * papel, tijera.
+    * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+    * - La función recibe un listado que contiene pares, representando cada jugada.
+    * - El par puede contener combinaciones de "R" (piedra), "P" (papel)
+    *   o "S" (tijera).
+    * - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
+    * 
+    * el emtodo debe devolver un string indicando el ganador.
+    */
+    public void ejercicio2(){
+        
+        
+    }
+}
